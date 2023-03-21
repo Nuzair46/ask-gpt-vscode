651578a9c6ad4905a6a7419373c0905b7f668373
Nuzair <nuzair.rasheed@bigbinary.com>
2023-03-21 18:48:38 +0530

    Added basic sidebar view

--- extension.js
+++ extension.js
@@ -1,36 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+const activate = (context) => {
+  const provider = {
+    resolveWebviewView: (webviewView) => {
+      webviewView.webview.options = { enableScripts: true };
+      webviewView.webview.html = `<!doctype><html><body><h1>Test</h1></body></html>`;
+    },
+  };
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ask-gpt-vscode" is now active!');
+  const panelView = vscode.window.registerWebviewViewProvider(
+    "ask-gpt.webView",
+    provider
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ask-gpt-vscode.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ask gpt vscode!');
-	});
-
-	context.subscriptions.push(disposable);
-}
+  context.subscriptions.push(panelView);
+};
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+const deactivate = () => {};
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
